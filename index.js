4062dd49184615b7a76c537b0ac75ffaff144b0d
Nadia <nadiamaksy4uk@gmail.com>
2023-01-15 01:22:01 +0900

    add switch button and some js-code

---
+++ index.js
@@ -0,0 +1,5 @@
+const switchButton = document.getElementById('switch');
+
+switchButton.addEventListener('click', function onClick(event) {
+    document.body.style.backgroundColor = '#bababa';
+  });
