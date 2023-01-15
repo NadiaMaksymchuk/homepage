b2d82d915cfa4934da0141aac19d22c2502fb6ac
Nadia <nadiamaksy4uk@gmail.com>
2023-01-15 19:24:16 +0900

    complete changing mode

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const switchButton = document.getElementById('switch');
 let popup = document.querySelector(".popup");
+let switcher = false;
 
 switchButton.addEventListener('click', function onClick(event) {
     let name = document.getElementById('name');
+    var turnedOffMode = 'Meow. The light had been turned off again!'
+    var turnedOnMode = 'Hooray, the light had been turned on, I can continue to sleep!'
 
-    document.body.style.backgroundColor = 'black';
+    if(!switcher) {
+      document.body.style.backgroundColor = 'black';
 
-    name.style.color = 'white';
+      name.style.color = 'white';
+  
+      openPop(turnedOffMode);
 
-    openPop();
-  });
+      switcher = true;
+    }
+    else {
+      document.body.style.backgroundColor = 'white';
 
-function openPop(){
-popup.style.display = "Block";
+      name.style.color = 'black';
+  
+      openPop(turnedOnMode);
+
+      switcher = false;
+    } 
+});
+
+function openPop(message){
+  var messegeParagraph = document.getElementById('message');
+
+  popup.style.display = "Block";
+  messegeParagraph.innerText=message;
 }
 
 window.addEventListener("click", closePop);
